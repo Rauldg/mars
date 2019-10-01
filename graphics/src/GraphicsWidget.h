1a462c71599710f789a730a203ee9c2d26d9dde4
Piyal93 <52539722+Piyal93@users.noreply.github.com>
2019-10-01 16:34:31 +0200

    Update GraphicsWidget.h

--- graphics/src/GraphicsWidget.h
+++ graphics/src/GraphicsWidget.h
@@ -183,8 +183,8 @@ namespace mars {
        * */
       virtual void getImageData(char *buffer, int &width, int &height);
       virtual void getImageData(void **data, int &width, int &height);
-      virtual void getTempImageData(char *buffer, int &width, int &height) = 0;
-      virtual void getTempImageData(void **data, int &width, int &height) = 0;
+      virtual void getTempImageData(char *buffer, int &width, int &height);
+      virtual void getTempImageData(void **data, int &width, int &height);
       /**
        * This function copies the depth image in the given buffer.
        * It assumes that the buffer ist correctly initalized
