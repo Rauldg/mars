c7053695c72ca3612a4f1329435d10f9d0850462
Piyal93 <52539722+Piyal93@users.noreply.github.com>
2019-10-08 15:44:27 +0200

    Update thermal_cam.h

--- graphics/src/thermal_cam.h
+++ graphics/src/thermal_cam.h
@@ -9,3 +9,15 @@
 #inclde "GraphicsWindowInterface.h"
 namespace mars {
   namespace graphics {
+
+      class thermal_cam {
+
+        public:
+       
+      virtual void getTempImageData(char *buffer, int &width, int &height);
+      //virtual void getTempImageData(void **data, int &width, int &height);
+
+      } // end of namespace interfaces
+} // end of namespace mars
+
+#endif  /* MARS_GRAPHICS_THERMAL_CAM_H */
