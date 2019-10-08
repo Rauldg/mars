bb67d2c517aaaa42aa7b79114d4aa8a7cc360749
Piyal93 <52539722+Piyal93@users.noreply.github.com>
2019-10-08 10:15:33 +0200

    Create thermal_cam.h

---
+++ graphics/src/thermal_cam.h
@@ -0,0 +1,11 @@
+#ifndef THERMAL_CAM_H
+#define THERMAL_CAM_H
+
+#ifdef _PRINT_HEADER_
+  #warning "thermal_cam.h"
+#endif
+#include "gui_helper_functions.h"
+#include "GraphicsCamera.h"
+#inclde "GraphicsWindowInterface.h"
+namespace mars {
+  namespace graphics {
